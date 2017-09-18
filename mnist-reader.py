668cb483e58fe557470beb8d244848b1d6d6cdeb
Rebecca <rebecca.kane@outlook.com>
2017-09-18 16:43:02 +0100

    Reading first 4 bytes in the file

--- mnist-reader.py
+++ mnist-reader.py
@@ -4,5 +4,9 @@ import gzip
 
 f = gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb')
 
-file_content = f.read()
-print(file_content)
+hexdata = f.read(4) 
+# Currently reads \x00\x00\x08\x03 -> 00000000 00000000 00001000 00000011 in binary
+
+# Use iPython to run through some options on hexdata, nothing of interest but - check if any options on int. int.from_bytes takes two arguments, try inputting hexdata and 'big' - big/little depends on which way binary is read by your machine (left to right or right to left)
+
+print(int.from_bytes(hexdata, 'big'))
