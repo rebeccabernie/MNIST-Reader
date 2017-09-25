0c4d7e08c42f8afb62dcc6ea7034a6a94a1b0da9
Rebecca <rebecca.kane@outlook.com>
2017-09-25 16:37:14 +0100

    Now reading from image files too

--- 1-ReadFile.py
+++ 1-ReadFile.py
@@ -4,30 +4,69 @@
 # Read gzip file adapted from https://stackoverflow.com/questions/12902540/read-from-a-gzip-file-in-python
 
 import gzip
-limit = 50 # Limiting output for now
-index = 9
 
-# Using "with" for files limits the scope of variables within the block
-with gzip.open('data/t10k-labels-idx1-ubyte.gz', 'rb') as f:
-    # Pointer at 0
-    magic = f.read(4)
-    magic = int.from_bytes(magic, 'big')
-    print("Magic: ", magic)
+def readLabelsFromFile(filename):
+    # Using "with" for files limits the scope of variables within the block
+    print("Label Files")
+    with gzip.open(filename, 'rb') as f:
+        # Pointer at 0
+        magic = f.read(4)
+        magic = int.from_bytes(magic, 'big')
+        print("(Magic: ", magic)
 
-    # Pointer now at 3 (4th pos)
-    numLabels = f.read(4)
-    numLabels = int.from_bytes(numLabels, 'big')
-    print("Num of labels: ", numLabels)
+        # Pointer now at 3 (4th pos)
+        numLabels = f.read(4)
+        numLabels = int.from_bytes(numLabels, 'big')
+        print("Num of labels: ", numLabels)
 
-    # Inefficient Way
-    # labels = []
+        # Inefficient Way
+        # labels = []
 
-    # for i in range(numLabels):
-    #     labels.append(f.read(1))
+        # for i in range(numLabels):
+        #     labels.append(f.read(1))
 
-    # Proper Way
-    # labels = [int.from_bytes(f.read(1)) for i in range(numLabels)]
-    # --------- OR, tidier ---------
-    labels = [f.read(1) for i in range(numLabels)]
-    labels = [int.from_bytes(label, 'big') for label in labels]
+        # Proper Way
+        # labels = [int.from_bytes(f.read(1)) for i in range(numLabels)]
+        # --------- OR, tidier ---------
+        labels = [f.read(1) for i in range(numLabels)]
+        labels = [int.from_bytes(label, 'big') for label in labels]
+    return labels
 
+trainLabels = readLabelsFromFile('data/train-labels-idx1-ubyte.gz')
+testLabels = readLabelsFromFile('data/t10k-labels-idx1-ubyte.gz')
+
+def readImagesFromFile(filename):
+    # Using "with" for files limits the scope of variables within the block
+    print()
+    print("Image Files")
+    with gzip.open(filename, 'rb') as f:
+        magic = f.read(4)
+        magic = int.from_bytes(magic, 'big')
+        print("Magic: ", magic)
+
+        numImgs = f.read(4)
+        numImgs = int.from_bytes(numImgs, 'big')
+        print("Num of Images: ", numImgs)
+
+        numRows = f.read(4)
+        numRows = int.from_bytes(numRows, 'big')
+        print("Num of Rows: ", numRows)
+
+        numCols = f.read(4)
+        numCols = int.from_bytes(numCols, 'big')
+        print("Num of Cols: ", numCols)
+
+        # Inefficient / Messy Way
+        images = []
+        for i in range(numImgs):
+            rows = []
+            for r in range(numRows):
+                cols = []
+                for c in range(numCols):
+                    cols.append(int.from_bytes(f.read(1), 'big'))
+                rows.append(cols)
+            images.append(rows)
+    return images
+
+trainImages = readImagesFromFile('data/train-images-idx3-ubyte.gz')
+testImages = readImagesFromFile('data/t10k-images-idx3-ubyte.gz')
