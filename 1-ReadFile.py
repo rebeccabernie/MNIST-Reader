5bce4e17ee7c5acd6e045a43b10dab7a14a02568
Rebecca <rebecca.kane@outlook.com>
2017-09-25 15:34:24 +0100

    Switching to labels instead of images, reading magic number & num of labels

--- 1-ReadFile.py
+++ 1-ReadFile.py
@@ -7,18 +7,13 @@ import gzip
 limit = 50 # Limiting output for now
 index = 9
 
-f = gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb')
+f = gzip.open('data/t10k-labels-idx1-ubyte.gz', 'rb')
 
+magic = f.read(4)
+print("Magic: " + str(int.from_bytes(magic, 'big')))
 
-for index, byte in enumerate(f):
-    hexdata = f.read(4) 
-    print("Hex: " + str(hexdata))
-    print("Int: " + str((int.from_bytes(hexdata, 'big')))) # Use iPython to run through some options on hexdata, nothing of interest but - check if any options on int. int.from_bytes takes two arguments, try inputting hexdata and 'big' - big/little depends on which way binary is read by your machine (left to right or right to left)
-    print()
-    
-    if index == limit:
-        break
-        # Limit & enumerate function adapted from https://stackoverflow.com/questions/36106712/how-can-i-limit-iterations-of-a-loop-in-python
+numLabels = f.read(4)
+print("Num of labels: " + str(int.from_bytes(numLabels, 'big')))
     
 # \x00\x00\x08\x03 (first item) -> 00000000 00000000 00001000 00000011 in binary
 
